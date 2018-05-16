88fba17e74d807375213f7a108e053db83fe6b3f
Kyle Evans <evans.584@osu.edu>
2018-05-16 12:18:17 -0400

    change order

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,16 +9,19 @@
 
 include_recipe 'kbe_role_ubuntu_1604_base'
 include_recipe 'kbe_perl'
-include_recipe 'kbe_nginx'
+
 
 template '/var/www/ddgsearch.pl' do
   source 'ddgsearch.pl.erb'
   owner 'root'
   group 'www-data'
   mode '0750'
+  notifies :reload, 'service[nginx]'
 end
 
 execute 'ddg_search_app' do
   command 'uwsgi --plugins psgi --socket 127.0.0.1:3031 --psgi /var/www/ddgsearch.pl'
   not_if "ps auxww | grep -v grep | grep 'uwsgi --plugins psgi --socket 127.0.0.1:3031 --psgi /var/www/ddgsearch.pl'"
 end
+
+include_recipe 'kbe_nginx'

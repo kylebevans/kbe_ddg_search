2009a53dcaf64933c4b1c669565b518492f9c83f
Kyle Evans <evans.584@osu.edu>
2018-05-16 11:33:39 -0400

    make psgi

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kylebe@gmail.com'
 license 'all_rights'
 description 'Installs/Configures kbe_ddg_search'
 long_description 'Installs/Configures kbe_ddg_search'
-version '0.1.0'
+version '0.1.1'
 
 depends 'kbe_role_ubuntu_1604_base'
 depends 'kbe_perl'
